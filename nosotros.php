332d1e7c7f6697837ae1566e23783b31232fc658
Mr. Coffee <charlieramirez675@gmail.com>
2025-04-03 19:16:47 -0600

    Add files via upload

---
+++ nosotros.php
@@ -0,0 +1,291 @@
+<?php
+include('db.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>HOTEL BAHIA DORADA</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Resort Inn Responsive , Smartphone Compatible web template , Samsung, LG, Sony Ericsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<link rel="stylesheet" href="css/chocolat.css" type="text/css" media="screen">
+<link href="css/easy-responsive-tabs.css" rel='stylesheet' type='text/css'/>
+<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" property="" />
+<link rel="stylesheet" href="css/jquery-ui.css" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<script type="text/javascript" src="js/modernizr-2.6.2.min.js"></script>
+<!--fonts-->
+<link href="//fonts.googleapis.com/css?family=Oswald:300,400,700" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Federo" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+<!--//fonts-->
+<script src="//code.tidio.co/pez7w0mdpljyddh38mbwjpegbrorwv5w.js" async></script>
+</head>
+<body>
+<!-- header -->
+<div class="banner-top">
+			<div class="social-bnr-agileits">
+				<ul class="social-icons3">
+								<li><a href="https://www.facebook.com/bahiadorada.hotesa?mibextid=wwXIfr&rdid=udWydJ5raCHV4wsK&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F15qP1DdeFv%2F%3Fmibextid%3DwwXIfr#" class="fa fa-facebook icon-border facebook"> </a></li>
+								
+							</ul>
+			</div>
+			<div class="contact-bnr-w3-agile">
+				<ul>
+					<li><i class="fa fa-envelope" aria-hidden="true"></i><a href="mailto:info@example.com">bahiadorada@hoteleselsalvador.com</a></li>
+					<li><i class="fa fa-phone" aria-hidden="true"></i>+503 7875 5594</li>	
+				</ul>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	<div class="w3_navigation">
+		<div class="container">
+			<nav class="navbar navbar-default">
+				<div class="navbar-header navbar-left">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<h1><a class="navbar-brand" href="index.php">BAHIA<span> DORADA</span><p class="logo_w3l_agile_caption">Hoteles Salvadoreños</p></a></h1>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+					<nav class="menu menu--iris">
+						<ul class="nav navbar-nav menu__list">
+							<li class="menu__item menu__item--current"><a href="index.php" class="menu__link">Inicio</a></li>
+							<li class="menu__item"><a href="index.php#about" class="menu__link scroll">Nosotros</a></li>
+							<li class="menu__item"><a href="index.php#gallery" class="menu__link scroll">Reseñas</a></li>
+							<li class="menu__item"><a href="index.php#rooms" class="menu__link scroll">Habitaciones</a></li>
+							<li class="menu__item"><a href="index.php#contact" class="menu__link scroll">Contactenos</a></li>
+						</ul>
+					</nav>
+				</div>
+			</nav>
+
+		</div>
+	</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+<div class="footer-completo">
+  <div class="footer-seccion">
+    <h3>Telefono Asistencia</h3>
+    <p>24 HORAS AL DIA</p>
+    <p>+503 2325 7500</p>
+    <p>+503 6062 6646</p>
+  </div>
+  
+  <div class="footer-seccion">
+    <h3>Conectate con nosotros</h3>
+    <div class="redes-sociales">
+      <a href="https://www.facebook.com/bahiadorada.hotesa?mibextid=wwXIfr&rdid=fmdNOFsiQZUYNbKH&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F15o8EpgQuU%2F%3Fmibextid%3DwwXIfr#"><img src="https://cdn-icons-png.flaticon.com/512/124/124010.png" alt="Facebook" width="30"></a>
+      <a href="https://www.instagram.com/bahiadorada.sv/"><img src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png" alt="Instagram" width="30"></a>
+      <a href="https://www.tiktok.com/@hoteles.elsalvador?is_from_webapp=1&sender_device=pc"><img src="https://cdn-icons-png.flaticon.com/512/3046/3046126.png" alt="TikTok" width="30"></a>
+    </div>
+    <h3>EMAIL</h3>
+    <p>reservaciones@hoteleselsalvador.com</p>
+    <p>bahtadorada@hoteleselsalvador.com</p>
+  </div>
+  
+  <div class="footer-seccion">
+    <h3>Boletín</h3>
+    <p>SUSCRIBETE PARA OFERTAS ESPECIALES</p>
+    <input type="email" placeholder="Ingresa tu email">
+	<button class="btn-suscribir">Suscribirse</button>
+
+  </div>
+</div>
+
+<div class="footer-inferior">
+  <p>Hotel Bahia Dorada <br>Todos los derechos reservados</p>
+  
+  <div class="enlaces-footer">
+    <a href="index.php">Inicio</a>
+    <a href="index.php#about">Nosotros</a>
+    <a href="index.php">Reseñas</a>
+    <a href="index.php#rooms">Habitaciones</a>
+    <a href="index.php#contact">Contactenos</a>
+    <a href="terminos.php">Términos y Condiciones</a>
+  </div>
+  
+  <p>Web Design by: DataFox. © 2025 Todos los derechos reservados</p>
+  
+  <div class="metodos-pago">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Visa_Inc._logo.svg/800px-Visa_Inc._logo.svg.png" alt="Visa" width="50">
+	<img src="images/logos-payments.png" alt="Visa" width="50">
+	<img src="images/fumar.png" alt="Visa" width="50">
+	<img src="images/perro.jpg" alt="Visa" width="50">
+  </div>
+</div>
+
+<style>
+.footer-completo {
+  background: #0f2453;
+  padding: 30px 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  font-family: Arial, sans-serif;
+  color: black;
+}
+
+.footer-seccion {
+  margin: 10px 20px;
+  min-width: 200px;
+}
+
+.footer-seccion h3 {
+  font-size: 16px;
+  margin-bottom: 10px;
+  color: #ffce14;
+  text-transform: uppercase;
+}
+
+.footer-seccion p {
+  font-size: 14px;
+  margin: 5px 0;
+  color: #ffff;
+}
+
+.redes-sociales {
+  display: flex;
+  gap: 10px;
+  margin: 10px 0;
+}
+
+.footer-seccion input[type="email"] {
+  width: 100%;
+  padding: 8px;
+  margin-top: 10px;
+  border: 1px solid #ddd;
+  
+}
+
+.btn-suscribir {
+  background-color: #ffce14;
+  border-radius:10px;
+  color: white;
+  border: none;
+  padding: 8px;
+  margin-top: 20px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+.btn-suscribir:hover {
+  background-color: #333;
+}
+.footer-inferior {
+  background: #e6e6e6;
+  padding: 20px;
+  text-align: center;
+  font-size: 12px;
+  color: #666;
+}
+
+.enlaces-footer {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 15px;
+  margin: 15px 0;
+}
+
+.enlaces-footer a {
+  color: #666;
+  text-decoration: none;
+}
+
+.metodos-pago {
+  margin-top: 15px;
+}
+
+/* Responsivo */
+@media (max-width: 768px) {
+  .footer-completo {
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+  }
+  
+  .redes-sociales {
+    justify-content: center;
+  }
+  
+  .enlaces-footer {
+    gap: 10px;
+  }
+}
+
+.payment-methods {
+            text-align: center;
+            margin-top: 15px;
+        }
+        
+        .payment-methods img {
+            max-width: 50px;
+            height: auto;
+        }
+        
+        @media (max-width: 768px) {
+            .footer-container {
+                grid-template-columns: 1fr;
+            }
+            
+            .footer-section {
+                text-align: center;
+            }
+            
+            .footer-links {
+                flex-direction: column;
+                align-items: center;
+                gap: 5px;
+            }
+        }
+</style>
+
+</body>
+</html>
---
+++ terminos.php
@@ -0,0 +1,348 @@
+<?php
+include('db.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>HOTEL BAHIA DORADA</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Resort Inn Responsive , Smartphone Compatible web template , Samsung, LG, Sony Ericsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<link rel="stylesheet" href="css/chocolat.css" type="text/css" media="screen">
+<link href="css/easy-responsive-tabs.css" rel='stylesheet' type='text/css'/>
+<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" property="" />
+<link rel="stylesheet" href="css/jquery-ui.css" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<script type="text/javascript" src="js/modernizr-2.6.2.min.js"></script>
+<!--fonts-->
+<link href="//fonts.googleapis.com/css?family=Oswald:300,400,700" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Federo" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+<!--//fonts-->
+<script src="//code.tidio.co/pez7w0mdpljyddh38mbwjpegbrorwv5w.js" async></script>
+</head>
+<body>
+<!-- header -->
+<div class="banner-top">
+			<div class="social-bnr-agileits">
+				<ul class="social-icons3">
+								<li><a href="https://www.facebook.com/bahiadorada.hotesa?mibextid=wwXIfr&rdid=udWydJ5raCHV4wsK&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F15qP1DdeFv%2F%3Fmibextid%3DwwXIfr#" class="fa fa-facebook icon-border facebook"> </a></li>
+								
+							</ul>
+			</div>
+			<div class="contact-bnr-w3-agile">
+				<ul>
+					<li><i class="fa fa-envelope" aria-hidden="true"></i><a href="mailto:info@example.com">bahiadorada@hoteleselsalvador.com</a></li>
+					<li><i class="fa fa-phone" aria-hidden="true"></i>+503 7875 5594</li>	
+				</ul>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	<div class="w3_navigation">
+		<div class="container">
+			<nav class="navbar navbar-default">
+				<div class="navbar-header navbar-left">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<h1><a class="navbar-brand" href="index.php">BAHIA<span> DORADA</span><p class="logo_w3l_agile_caption">Hoteles Salvadoreños</p></a></h1>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+					<nav class="menu menu--iris">
+						<ul class="nav navbar-nav menu__list">
+							<li class="menu__item menu__item--current"><a href="index.php" class="menu__link">Inicio</a></li>
+							<li class="menu__item"><a href="index.php#about" class="menu__link scroll">Nosotros</a></li>
+							<li class="menu__item"><a href="index.php#gallery" class="menu__link scroll">Reseñas</a></li>
+							<li class="menu__item"><a href="index.php#rooms" class="menu__link scroll">Habitaciones</a></li>
+							<li class="menu__item"><a href="index.php#contact" class="menu__link scroll">Contactenos</a></li>
+						</ul>
+					</nav>
+				</div>
+			</nav>
+
+		</div>
+	</div>
+
+
+
+
+
+<style>
+    .terms-container {
+        max-width: 800px;
+        margin: 0 auto;
+        padding: 20px;
+        font-family: Arial, sans-serif;
+        line-height: 1.6;
+        color: #333;
+    }
+    
+    .terms-title {
+        color: #2c3e50;
+        text-align: center;
+        margin-bottom: 30px;
+    }
+    
+    .terms-section {
+        margin-bottom: 30px;
+    }
+    
+    .terms-section h2 {
+        color: #2980b9;
+        margin-top: 25px;
+        border-bottom: 1px solid #eee;
+        padding-bottom: 5px;
+    }
+    
+    .terms-intro {
+        font-size: 1.1em;
+        margin-bottom: 30px;
+    }
+</style>
+
+<div class="terms-container">
+    <h1 class="terms-title">Términos y Condiciones de Uso</h1>
+    
+    <p class="terms-intro">Bienvenido a la página web de Hotel Bahia Dorada. Al acceder y utilizar nuestro sitio web, aceptas los siguientes términos y condiciones, así como nuestras políticas de privacidad. Este sitio proporciona información y servicios relacionados con nuestros hoteles.</p>
+    
+    <div class="terms-section">
+        <h2>1. Uso del sitio web</h2>
+        <p>Este sitio web está diseñado para facilitar información sobre nuestros hoteles, servicios, reservas y promociones. Al usar este sitio, garantizas que tienes al menos 18 años de edad y que utilizarás los servicios de manera legal y responsable.</p>
+        <p>No se permite el uso de nuestro sitio para fines ilegales o no autorizados, como intentar acceder a nuestras bases de datos, suplantación de identidad o cualquier otra actividad que comprometa la seguridad del sitio.</p>
+    </div>
+    
+    <div class="terms-section">
+        <h2>2. Reservas y pagos</h2>
+        <p>Todas las reservas están sujetas a disponibilidad y deben realizarse con los datos correctos. Nos reservamos el derecho de rechazar una reserva si la información proporcionada es incorrecta o si no se cumplen los requisitos necesarios.</p>
+        <p>Al realizar una reserva, se proporcionarán detalles específicos sobre los términos de pago y las políticas de cancelación aplicables a la tarifa seleccionada.</p>
+        <p>Cualquier pago realizado en línea se procesará a través de un sistema seguro y encriptado para proteger tus datos.</p>
+        <p>Devoluciones se realizarán vía transferencia bancaria o reembolso en tarjeta de crédito.</p>
+    </div>
+    
+    <div class="terms-section">
+        <h2>3. Promociones y ofertas</h2>
+        <p>Las promociones, descuentos y ofertas especiales están sujetos a condiciones específicas. Nos reservamos el derecho de modificar, suspender o terminar cualquier promoción en cualquier momento sin previo aviso.</p>
+    </div>
+    
+    <div class="terms-section">
+        <h2>4. Protección de datos</h2>
+        <p>Respetamos la privacidad de nuestros usuarios y protegemos los datos personales de acuerdo con las leyes locales e internacionales.</p>
+    </div>
+    
+    <div class="terms-section">
+        <h2>5. Propiedad intelectual</h2>
+        <p>Todo el contenido en este sitio, incluidos textos, imágenes, logotipos, y cualquier otro material, es propiedad exclusiva de Hotel Bahia Dorada. Está prohibido copiar, distribuir o utilizar este contenido sin nuestro consentimiento previo por escrito.</p>
+    </div>
+    
+    <div class="terms-section">
+        <h2>6. Limitación de responsabilidad</h2>
+        <p>No asumimos responsabilidad por daños directos, indirectos o incidentales derivados del uso de nuestro sitio o de los servicios de nuestro hotel. Nos esforzamos por garantizar que la información en el sitio esté actualizada, pero no garantizamos la exactitud o completitud de los contenidos.</p>
+    </div>
+    
+    <div class="terms-section">
+        <h2>7. Modificaciones a los términos y condiciones</h2>
+        <p>Hotel Bahia reserva el derecho de modificar estos términos en cualquier momento. Es tu responsabilidad revisar periódicamente los términos y condiciones actualizados.</p>
+    </div>
+    
+    <div class="terms-section">
+        <h2>8. Política de Cancelación</h2>
+        <p>Las cancelaciones deben realizarse a través de nuestro sitio web o comunicándose directamente con nuestro equipo de atención al cliente. Estas deben presentarse dentro de los 3 días posteriores a la fecha de compra para ser consideradas sin cargos.</p>
+        <p>Para procesar la cancelación sin penalización, es indispensable presentar una justificación válida, la cual será evaluada por nuestro equipo de atención al cliente.</p>
+    </div>
+    
+    <div class="terms-section">
+        <h2>9. Contacto</h2>
+        <p>Si tienes preguntas o necesitas más información sobre nuestros términos y condiciones, puedes contactarnos a través de nuestra página de contacto o llamarnos al +503 2526 7000.</p>
+    </div>
+</div>
+
+
+<div class="footer-completo">
+  <div class="footer-seccion">
+    <h3>Telefono Asistencia</h3>
+    <p>24 HORAS AL DIA</p>
+    <p>+503 2325 7500</p>
+    <p>+503 6062 6646</p>
+  </div>
+  
+  <div class="footer-seccion">
+    <h3>Conectate con nosotros</h3>
+    <div class="redes-sociales">
+      <a href="https://www.facebook.com/bahiadorada.hotesa?mibextid=wwXIfr&rdid=fmdNOFsiQZUYNbKH&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F15o8EpgQuU%2F%3Fmibextid%3DwwXIfr#"><img src="https://cdn-icons-png.flaticon.com/512/124/124010.png" alt="Facebook" width="30"></a>
+      <a href="https://www.instagram.com/bahiadorada.sv/"><img src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png" alt="Instagram" width="30"></a>
+      <a href="https://www.tiktok.com/@hoteles.elsalvador?is_from_webapp=1&sender_device=pc"><img src="https://cdn-icons-png.flaticon.com/512/3046/3046126.png" alt="TikTok" width="30"></a>
+    </div>
+    <h3>EMAIL</h3>
+    <p>reservaciones@hoteleselsalvador.com</p>
+    <p>bahtadorada@hoteleselsalvador.com</p>
+  </div>
+  
+  <div class="footer-seccion">
+    <h3>Boletín</h3>
+    <p>SUSCRIBETE PARA OFERTAS ESPECIALES</p>
+    <input type="email" placeholder="Ingresa tu email">
+	<button class="btn-suscribir">Suscribirse</button>
+
+  </div>
+</div>
+
+<div class="footer-inferior">
+  <p>Hotel Bahia Dorada <br>Todos los derechos reservados</p>
+  
+  <div class="enlaces-footer">
+    <a href="index.php">Inicio</a>
+    <a href="index.php#about">Nosotros</a>
+    <a href="index.php">Reseñas</a>
+    <a href="index.php#rooms">Habitaciones</a>
+    <a href="index.php#contact">Contactenos</a>
+    <a href="terminos.php">Términos y Condiciones</a>
+  </div>
+  
+  <p>Web Design by: DataFox. © 2025 Todos los derechos reservados</p>
+  
+  <div class="metodos-pago">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Visa_Inc._logo.svg/800px-Visa_Inc._logo.svg.png" alt="Visa" width="50">
+	<img src="images/logos-payments.png" alt="Visa" width="50">
+	<img src="images/fumar.png" alt="Visa" width="50">
+	<img src="images/perro.jpg" alt="Visa" width="50">
+  </div>
+</div>
+
+<style>
+.footer-completo {
+  background: #0f2453;
+  padding: 30px 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  font-family: Arial, sans-serif;
+  color: black;
+}
+
+.footer-seccion {
+  margin: 10px 20px;
+  min-width: 200px;
+}
+
+.footer-seccion h3 {
+  font-size: 16px;
+  margin-bottom: 10px;
+  color: #ffce14;
+  text-transform: uppercase;
+}
+
+.footer-seccion p {
+  font-size: 14px;
+  margin: 5px 0;
+  color: #ffff;
+}
+
+.redes-sociales {
+  display: flex;
+  gap: 10px;
+  margin: 10px 0;
+}
+
+.footer-seccion input[type="email"] {
+  width: 100%;
+  padding: 8px;
+  margin-top: 10px;
+  border: 1px solid #ddd;
+  
+}
+
+.btn-suscribir {
+  background-color: #ffce14;
+  border-radius:10px;
+  color: white;
+  border: none;
+  padding: 8px;
+  margin-top: 20px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+.btn-suscribir:hover {
+  background-color: #333;
+}
+.footer-inferior {
+  background: #e6e6e6;
+  padding: 20px;
+  text-align: center;
+  font-size: 12px;
+  color: #666;
+}
+
+.enlaces-footer {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 15px;
+  margin: 15px 0;
+}
+
+.enlaces-footer a {
+  color: #666;
+  text-decoration: none;
+}
+
+.metodos-pago {
+  margin-top: 15px;
+}
+
+/* Responsivo */
+@media (max-width: 768px) {
+  .footer-completo {
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+  }
+  
+  .redes-sociales {
+    justify-content: center;
+  }
+  
+  .enlaces-footer {
+    gap: 10px;
+  }
+}
+
+.payment-methods {
+            text-align: center;
+            margin-top: 15px;
+        }
+        
+        .payment-methods img {
+            max-width: 50px;
+            height: auto;
+        }
+        
+        @media (max-width: 768px) {
+            .footer-container {
+                grid-template-columns: 1fr;
+            }
+            
+            .footer-section {
+                text-align: center;
+            }
+            
+            .footer-links {
+                flex-direction: column;
+                align-items: center;
+                gap: 5px;
+            }
+        }
+</style>
+
+</body>
+</html>
